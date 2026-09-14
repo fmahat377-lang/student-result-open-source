4fdbcb2c7827e1f06676cf2b93a5e2dcfae3b15f
Fatima <fmahat377@gmail.com>
2026-09-14 19:29:59 +0530

    Update passing marks to 60

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkResult(){
     let name=document.getElementById("name").value;
     let marks=document.getElementById("marks").value;
-    if(marks>=50){
+    if(marks>=60){
         document.getElementById("result").innerHTML=name+"-PASS";
     }else{
         document.getElementById("result").innerHTML=name+"-FAIL";
